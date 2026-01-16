874221dc9204962e50afaf4ce392b76e64dcd8b8
Heena Maheshwari <158318184+HeenaMaheshwari@users.noreply.github.com>
2026-01-17 00:37:47 +0530

    Update silver_transformation.sql

--- project-1-batch-etl/silver_transformation.sql
+++ project-1-batch-etl/silver_transformation.sql
@@ -1,2 +1,13 @@
 -- Silver layer transformations will be added here
+-- Create Silver table with cleaned and transformed data
+CREATE OR REPLACE TABLE silver_sales AS
+SELECT
+    order_id,
+    customer_id,
+    CAST(order_date AS DATE) AS order_date,
+    CAST(amount AS DOUBLE) AS amount,
+    region
+FROM delta.`/mnt/bronze/sales/`
+WHERE order_id IS NOT NULL
+  AND amount IS NOT NULL;
 
