2d05d27c839145c4d00f15a29b0d4f4b442a18c1
Heena Maheshwari <158318184+HeenaMaheshwari@users.noreply.github.com>
2026-01-17 00:39:36 +0530

    Update gold_aggregation.sql

--- project-1-batch-etl/gold_aggregation.sql
+++ project-1-batch-etl/gold_aggregation.sql
@@ -1,1 +1,9 @@
--- Gold layer aggregations will be added here
+-- Create Gold table with business-ready aggregations
+CREATE OR REPLACE TABLE gold_sales_summary AS
+SELECT
+    region,
+    order_date,
+    SUM(amount) AS total_sales,
+    COUNT(order_id) AS total_orders
+FROM silver_sales
+GROUP BY region, order_date;
